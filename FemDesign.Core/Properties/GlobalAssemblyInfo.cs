649688ef2d3d544555549bbbcb386438a37c3626
Alexander Radne <32938476+xRadne@users.noreply.github.com>
2022-12-02 11:33:32 +0100

    Test

--- FemDesign.Core/Properties/GlobalAssemblyInfo.cs
+++ FemDesign.Core/Properties/GlobalAssemblyInfo.cs
@@ -23,5 +23,5 @@ using System.Runtime.InteropServices;
 //      Revision
 //
 
-[assembly: AssemblyVersion("21.6.0.0")]
-[assembly: AssemblyFileVersion("21.6.0.0")]
+[assembly: AssemblyVersion("0.0.0.1")]
+[assembly: AssemblyFileVersion("0.0.0.1")]
